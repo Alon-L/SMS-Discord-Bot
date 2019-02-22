11b7a3af5c66becfc004d24985f48f7fb6f351aa
alon <2005alonlivne@gmail.com>
2019-02-22 20:16:05 +0200

    Updated notes.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Libraries
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const http = require('http');
@@ -13,21 +14,30 @@ const countries = require ('./libs/PhonePrefixes/main.json');
 let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
+/**
+ * @desc Post route for the sms receiver.
+ */
 app.post('/sms', function(req, res) {
     let author = req.body.author;
     let message = req.body.message;
 
+    // Checks if the author is a number or a company/person name.
     if (!numberIsNaN(author)) {
+        // Gets the country prefix, name and code.
         let country = countryPrefix(author);
         if (country) {
+            // Sets the author to a more detailed string.
             author = `${country.name} 0${author.slice(country.prefix.length).match(/.{1,3}/g).join('-')}`;
         }
     }
 
+    // Gets guild
     let guild = client.guilds.get(conf.guildId);
     if (guild) {
+        // Gets channel
         let channel = guild.channels.find(c => c.type === 'text' && c.name.toLowerCase() === conf.channel.toLowerCase());
         if (channel) {
+            // Sends embed
             let embed = new Discord.RichEmbed()
                 .setTitle(`${author} | ${new Date(Date.now() + conf.hours).toUTCString()}`)
                 .setDescription(message)
@@ -39,6 +49,11 @@ app.post('/sms', function(req, res) {
     res.end();
 });
 
+/**
+ * @desc Returns an object including the country code, prefix and country name by the entered phone number.
+ * @param {String} num - The author phone number.
+ * @return {{code: String, prefix: String, name: String}}
+ */
 function countryPrefix(num) {
     let prefix = num.slice(0, -(num.length - 4));
     let countryPrefix;
@@ -51,13 +66,20 @@ function countryPrefix(num) {
     return countryPrefix;
 }
 
+/**
+ * @desc Checks if the number is a name or an actual phone number.
+ * @param {String} num - The author phone number.
+ * @return {boolean}
+ */
 function numberIsNaN(num) {
     return isNaN(num.slice(1));
 }
 
+// Sets port and creates a new http server.
 const port = 3000;
 http.createServer(app).listen(port, function () {
     console.log('Running server on port %s', port);
 });
 
+// Logs in the Discord client.
 client.login(require('./token.json').token);
